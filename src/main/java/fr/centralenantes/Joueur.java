2a4643aef1df83cedc73cfd6d57b0f72c8128f60
Yassss785 <yasminebouhadji02@gmail.com>
2025-11-10 14:45:41 +0100

    modif

--- src/main/java/fr/centralenantes/Joueur.java
+++ src/main/java/fr/centralenantes/Joueur.java
@@ -10,5 +10,11 @@ package fr.centralenantes;
  * @author yasminebouhadji
  */
 public class Joueur {
+    private String nom; // nom du joueur
+    private int fortune; // fortune du joueur
+    private int position; // position actuelle du joueur sur le plateau
+    private Plateau plateau; // référence au plateau de jeu
+
+
 
 }
